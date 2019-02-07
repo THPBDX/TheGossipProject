38cac17a0633e8d522966d619a4cb0cd439dd2ad
Estebanpetaflop <etienne.chev@gmail.com>
2019-02-07 20:31:32 +0100

    v avec sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'contact#show'
   get '/team', to: 'team#show'
 
-<<<<<<< HEAD
 resources :sessions
 
 resources :users
@@ -14,12 +13,5 @@ resources :gossips
 resources :comments
 resources :subcomments
 resources :likes
-=======
-  
-
-  resources :gossips
-  resources :users
-  resources :cities
->>>>>>> master
 
   end
